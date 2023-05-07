b10e685949fc34b180955131dc5c99e42280ab7a
kaaang-shin <shinhyeok.kang@dshare.co.kr>
2023-05-08 00:00:39 +0900

    Section8-4: Binder

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,5 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,20 +10,16 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath:}")
-    String contextPath;
-
-    @Value("${port:8080}")
-    int port;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(){
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties){
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
 
-        factory.setContextPath(this.contextPath);
-        factory.setPort(this.port);
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
 
         return factory;
     }
+
+
 }
