fa7928a224aaffcecf450a07678e71b69b6485ef
kaaang-shin <shinhyeok.kang@dshare.co.kr>
2023-05-07 17:48:52 +0900

    Section8-2: environment

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -1,21 +1,10 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HelloBootApplication {
-
-    @Bean
-    ApplicationRunner applicationRunner(Environment env){
-        return args -> {
-            String name = env.getProperty("my.name");
-            System.out.println("my.name: " + name);
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloBootApplication.class, args);
